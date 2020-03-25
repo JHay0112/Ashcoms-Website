a2d19dc6ea6d46784c65ebec18c4167d33642fef
JHay0112 <jordanhay0112@gmail.com>
2020-03-25 15:22:27 +1300

    Header

--- js/main.js
+++ js/main.js
@@ -21,3 +21,36 @@ async function exitLoadingScreen() {
     await sleep(1200);
     document.body.style.overflowY = "auto";
 }
+
+// Slideshow
+async function runSlideShow() {
+
+    images = [
+        "1.jpg",
+        "2.jpg"
+    ];
+
+    header = document.getElementById("header");
+    pseudoHeader = document.getElementById("pseudo-header");
+
+    imageURL = "url('img/header/";
+    imageURL = imageURL.concat(images[0], "')");
+
+    for(var i = 0; i <= images.length; i++) {
+
+        if(i >= images.length) {
+            i = 0;
+        }
+
+        pseudoHeader.style.backgroundImage = "url(\"img/header/".concat(images[i], "\")");
+        await sleep(500);
+        pseudoHeader.style.display = "inline-block";
+        pseudoHeader.style.animation = "slideshow-slide-in 1s ease";
+        await sleep(1000);
+        header.style.backgroundImage = "url(\"img/header/".concat(images[i], "\")");
+        pseudoHeader.style.animation = "";
+        pseudoHeader.style.display = "none";
+        await sleep(8000);
+    }
+
+}
